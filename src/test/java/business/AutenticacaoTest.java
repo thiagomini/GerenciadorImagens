43af3799590777f3360178c69361b3607b9358c8
Miqueas Amorim <miqueas.at@gmail.com>
2020-12-13 15:24:51 -0300

    Novos CTs para Autenticacao de usuário

--- src/test/java/business/AutenticacaoTest.java
+++ src/test/java/business/AutenticacaoTest.java
@@ -11,6 +11,7 @@ import repository.UsuarioRepository;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class AutenticacaoTest {
@@ -42,5 +43,34 @@ public class AutenticacaoTest {
 
         Usuario usuarioAutenticado = this.autenticacao.autenticar(usuario.getEmail(), usuario.getPassword());
         assertEquals(usuario, usuarioAutenticado);
+    }
+
+
+    /**
+     * Função <b>autenticar(email, password)</b>
+     * Deve ser levantado um erro com a mensagem “E-mail ou senha estão incorretos.”
+     * ao tentar autenticar um usuário não cadastrado
+     */
+    @Test
+    void CT035() {
+        RuntimeException exception = assertThrows(RuntimeException.class,
+                () -> this.autenticacao.autenticar("Usuario Inexistente", "123456"));
+        assertEquals("E-mail ou senha estão incorretos.", exception.getMessage());
+    }
+
+    /**
+     * Função <b>autenticar(email, password)</b>
+     * Deve ser levantado um erro com a mensagem “E-mail ou senha estão incorretos.”
+     * ao tentar autenticar um usuário cuja senha informada está incorreta
+     */
+    @Test
+    void CT036() {
+        Cargo cargo = new Cargo("Usuario", "user");
+        Usuario usuario = new Usuario("Fulano", "fulano@mail.com" , "1234", cargo);
+        Optional<Usuario> usuarioOptional = this.usuarioRepository.save(usuario);
+
+        RuntimeException exception = assertThrows(RuntimeException.class,
+                () -> this.autenticacao.autenticar(usuario.getEmail(), "abc"));
+        assertEquals("E-mail ou senha estão incorretos.", exception.getMessage());
     }
 }
